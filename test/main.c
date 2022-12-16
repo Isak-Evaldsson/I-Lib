8576d0d2a059e339b21d4be65e270a4540626df6
Isak Evaldsson <isak.evaldsson@outlook.com>
2022-12-16 16:28:16 +0100

    Better unit test

--- test/main.c
+++ test/main.c
@@ -4,10 +4,9 @@
 
 #include "ilib.h"
 
-void vec_test()
+void vec_test_fill()
 {
-    printf("Vector tests\n");
-
+    printf("Fill array...");
     size_t    i, j;
     int     **ptr;
     int       data[100];
@@ -25,32 +24,61 @@ void vec_test()
     }
     assert(i == count);
     vector_free(vec);
-    printf("Success tests\n");
+    printf("Success\n");
+}
+
+void list_test_push_pop()
+{
+    size_t n;
+    int    e[] = {1, 2, 3};
+
+    list_t *list = NULL;
+
+    printf("Push and pop...");
+
+    // Initial insert
+    list_push(&list, e);
+    assert(list->next = list);
+    assert(list->prev = list);
+
+    list_push(&list, e + 1);
+    list_push(&list, e + 2);
+    assert(list->next->next = list);
+    assert(list->next->next = list->prev);
+    assert((n = list_size(list)) == 3);
+
+    for (int i = 0; i < n; i++) {
+        int *elem = list_pop(&list);
+        assert(*elem == e[i]);
+    }
+
+    // List should be empty efter poping
+    assert(list == NULL);
+    printf("Success\n");
+}
+
+void list_test_free()
+{
+    printf("Free list...");
+    list_t *list = NULL;
+    size_t  n    = 100;
+
+    for (size_t i = 0; i < n; i++) {
+        list_push(&list, NULL);
+    }
+    assert(list_size(list) == n);
+    list_free(&list);
+    assert(list == NULL);
+
+    printf("Success\n");
 }
 
 int main()
 {
-    vec_test();
-    // Test elements
-    // int count = 0;
-    // int e[] = {1, 2, 3};
+    printf("Vector tests:\n");
+    vec_test_fill();
 
-    // list_t *list = list_empty();
-    // list_push(list, e);
-    // list_push(list, e + 1);
-    // list_push(list, e + 2);
-
-    // printf("Test %d\n", count);
-
-    // for (int *elem = list_iter_begin(list); elem != NULL; list_iter_next(list))
-    // {
-    //     printf("Elem %d\n", *elem);
-    //     // assert(*elem == e[count]);
-    //     count++;
-    // }
-
-    // // list_pop(list);
-    // // list_pop(list);
-    // // list_pop(list);
-    // list_free(list);
+    printf("\nList tests:\n");
+    list_test_push_pop();
+    list_test_free();
 }
