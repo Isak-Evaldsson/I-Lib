e65c43ba55e9a9bc92b26077ddd92ba60568abe0
Isak Evaldsson <isak.evaldsson@outlook.com>
2022-12-16 16:28:03 +0100

    Simplifed working list implementaion

--- include/ilib/list.h
+++ include/ilib/list.h
@@ -5,24 +5,30 @@
 #define ILIB_LIST_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
 /**
- * @brief Generic linked list implementation, giving a broad range of methods for list manipulation.
- * Implements interator methods, allowing fast list iteration without exposing the internal list
- * implementation, similar to other languages with built in iterator support.
+ * @brief Generic circular doubly linked list
  */
 typedef struct list_t list_t;
 
+struct list_t {
+    void   *data;
+    list_t *next;
+    list_t *prev;
+};
+
 /**
- * Creates empty list
- * @return the new list
+ * Creates new list containg one item
+ * @param data the data to add to the list
+ * @return the new list, NULL if failure
  */
-list_t *list_empty();
+list_t *list_new(void *data);
 
 /**
  * @brief Frees the memory of the list, however, it does not free the list contents
  */
-void list_free(list_t *list);
+void list_free(list_t **list);
 
 /**
  * Adds element to end of list
@@ -30,55 +36,18 @@ void list_free(list_t *list);
  * @param data - the data to add to the list. Trying to add NULL elements will results in failure.
  * @returns if successful operations
  */
-bool list_push(list_t *list, void *data);
+bool list_push(list_t **list, void *data);
 
 /**
  * Pops element at the beginning of the list
  * @param list - the list to pop from
  * @returns the popped element, NUll if list is empty
  */
-void *list_pop(list_t *list);
+void *list_pop(list_t **list);
 
 /**
- * @brief Resets list iterator and returns the first element of the list.
- * @param list - the list that's iterated
- * @returns the first element of the list, NULL if list is empty */
-void *list_iter_begin(list_t *list);
-
-/**
- * @brief Gives next element of the list
- * @param list - the list that's iterated
- * @returns the next element of the list, NULL if we have reached the end of the list
+ * Gets numbers of elements in list
  */
-void *list_iter_next(list_t *list);
-
-/**
- * @brief Removes the element returned form the last iterator call (list_iter_next(...) or
- * list_iter_begin(...)). If called before list_iter_begin(...) or after we reached end of list,
- * undefined behavior.
- * @param list - the list that's iterated
- * @return the removed element
- */
-void *list_iter_remove(list_t *list);
-
-/**
- * @brief Inserts data before the element returned form the last iterator call (list_iter_next(...)
- * or list_iter_begin(...)). If called before list_iter_begin(...) or after we reached end of list,
- * undefined behavior.
- * @param list - the list that's iterated
- * @param data - the data to insert, trying to add NULL elements will results in failure.
- * @returns if insertion is successful
- */
-bool list_iter_insert_before(list_t *list, void *data);
-
-/**
- * @brief Inserts data after the element returned form the last iterator call (list_iter_next(...)
- * or list_iter_begin(...)). If called before list_iter_begin(...) or after we reached end of list,
- * undefined behavior.
- * @param list - the list that's iterated
- * @param data - the data to insert, trying to add NULL elements will results in failure.
- * @returns if insertion is successful
- */
-bool list_iter_insert_after(list_t *list, void *data);
+size_t list_size(list_t *list);
 
 #endif /* ILIB_LIST_H */
--- src/list.c
+++ src/list.c
@@ -3,124 +3,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-    Internal data structures
-*/
-typedef struct list_node_t list_node_t;
-
-struct list_node_t {
-    void        *data;
-    list_node_t *next;
-    list_node_t *prev;
-};
-
-struct list_t {
-    /* Pointers to start/end of list*/
-    list_node_t *head;
-    list_node_t *tail;
-
-    // Current iterator object
-    list_node_t *iter;
-};
-
-list_t *list_empty()
+list_t *list_new(void *data)
 {
     list_t *list = calloc(1, sizeof(list_t));
+    if (list == NULL) {
+        return NULL;
+    }
+    list->data = data;
+    list->prev = list->next = list;
+
     return list;
 }
 
-void list_free(list_t *list)
+void list_free(list_t **list)
 {
-    list_node_t *node;
-    list_node_t *next = list->head;
+    list_t *head = *list;
+    list_t *next = head;
+    list_t *tmp;
 
-    // free list elements
-    while (next != NULL) {
-        node = next;
-        next = next->next;
-        free(node);
+    if (head == NULL) {
+        return;
     }
 
-    free(list);
+    do {
+        tmp  = next;
+        next = next->next;
+        free(tmp);
+    } while (next != head);
+
+    *list = NULL;
 }
 
-bool list_push(list_t *list, void *data)
+bool list_push(list_t **list, void *data)
 {
-    list_node_t *new;
+    list_t *head = *list;
+    list_t *new  = list_new(data);
+    list_t *tmp;
 
-    if (data == NULL || (new = calloc(1, sizeof(list_node_t))) == NULL) {
+    if (new == NULL) {
         return false;
     }
 
-    // prepend
-    new->data  = data;
-    new->next  = list->head;
-    list->head = new;
-
-    // set head = tail if list was previously empty
-    if (list->tail == NULL) {
-        list->tail = new;
+    if (head == NULL) {
+        *list = new;
+        return true;
     }
+
+    tmp        = head->prev;  // previous last element
+    tmp->next  = new;         // set new to last
+    new->next  = head;        // make ciruclar reference to head
+    new->prev  = tmp;         // set new to succed tmp
+    head->prev = new;         // make ciruclar reference to new
 
     return true;
 }
 
-void *list_pop(list_t *list)
+void *list_pop(list_t **list)
 {
-    void        *data = NULL;
-    list_node_t *node = list->tail;
+    list_t *head = *list;
+    list_t *tmp;
+    void   *data;
 
-    // Empty list
-    if (node == NULL) goto end;
-
-    // Single list entry
-    if (node == list->head) {
-        // make list empty
-        list->head = (list->tail = NULL);
-        goto end;
+    if (head == NULL) {
+        return NULL;
     }
 
-    // Remove last object from list
-    data             = node->data;
-    node->prev->next = NULL;
-    list->tail       = node->prev;
+    // 1 element in list;
+    if (head == head->next) {
+        *list = NULL;
+    } else {
+        tmp              = head->next;  // new head
+        tmp->prev        = head->prev;  // tmp->prev = last
+        head->prev->next = tmp;         // last->next = tmp
+        *list            = tmp;         // set head
+    }
 
-end:
-    free(node);
+    data = head->data;
+    free(head);
+
     return data;
 }
 
-void *list_iter_begin(list_t *list)
+size_t list_size(list_t *list)
 {
-    if (list->head == NULL) {
-        return NULL;
+    list_t *head = list;
+    list_t *next = head;
+    size_t  size = 0;
+
+    if (head == NULL) {
+        return size;
     }
 
-    list->iter = list->head;
-    return list->iter->data;
-}
+    do {
+        size++;
+        next = next->next;
+    } while (next != head);
 
-void *list_iter_next(list_t *list)
-{
-    if (list->iter->next == NULL) {
-        return NULL;
-    }
-
-    list->iter = list->iter->next;
-    return list->iter->data;
-}
-
-void *list_iter_remove(list_t *list)
-{
-    list_node_t *node = list->iter;
-
-    // handle first element/empty list/one item list
-    if (list->head == node) return list_pop(list);
-
-    // remove entry
-    node->prev->next = node->next;
-    node->next->prev = node->prev;
-
-    free(node);
-    return node->data;
+    return size;
 }
